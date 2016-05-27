934c3fbe1676b9be96ccafc8203cb82dd84606d5
jleewebdev <lee45276@gmail.com>
2016-05-27 14:27:36 +0800

    Tests pass

---
+++ word_count.rb
@@ -0,0 +1,25 @@
+class Phrase
+
+  attr_accessor :word_count
+
+  def initialize(phrase)
+    @word_count = {}
+    to_hash(phrase)
+    
+  end
+
+  def to_hash(phrase)
+    phrase.gsub(/[^\w\s,\']/, "").split(/\s|,/).each do |word|
+      next if word == ""
+      word = word.downcase
+      word[0] = "" if word[0] == "'"
+      word[-1] = "" if word[-1] == "'"
+      if @word_count[word]
+        @word_count[word] += 1
+      else
+        @word_count[word] = 1
+      end
+    end
+  end
+
+end
